6e68260b528b14f543f650026fef1656aa791245
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-02-13 10:17:05 +0000

    Implemented product model

--- MMTShop.Shared/Models/Product.cs
+++ MMTShop.Shared/Models/Product.cs
@@ -8,6 +8,9 @@ namespace MMTShop.Shared.Models
 {
     public class Product
     {
-        
+        public int Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
