7f78a5a8ba4f11ed983fc7d497dc5c2f20b64cea
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-02-13 10:48:04 +0000

    Implemented Get Products by Category feature

--- MMTShop.Server/Features/Product/GetProductsByCategory/GetProductsByCategoryHandler.cs
+++ MMTShop.Server/Features/Product/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Dapper;
+using MMTShop.Server.Base;
+using MMTShop.Shared.Constants;
+using MMTShop.Shared.Contracts;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MMTShop.Server.Features.Product.GetProductsByCategory
 {
     public class GetProductsByCategoryHandler
+        : DbRequestHandlerBase<GetProductsByCategoryRequest, GetProductsByCategoryResponse>
     {
-        
+        public override async Task<GetProductsByCategoryResponse> Handle(GetProductsByCategoryRequest request, CancellationToken cancellationToken)
+        {
+            var products = await DbConnection
+                .QueryAsync<Shared.Models.Product>(DataAccess
+                    .GetCommand(DataConstants.GetFeaturedProducts));
+
+            return new GetProductsByCategoryResponse { Products = products };
+        }
+
+        public GetProductsByCategoryHandler(
+            IDbConnection dbConnection, 
+            IDataAccess dataAccess) 
+            : base(dbConnection, 
+                   dataAccess)
+        {
+        }
+
     }
 }
--- MMTShop.Server/Features/Product/GetProductsByCategory/GetProductsByCategoryResponse.cs
+++ MMTShop.Server/Features/Product/GetProductsByCategory/GetProductsByCategoryResponse.cs
@@ -8,6 +8,6 @@ namespace MMTShop.Server.Features.Product.GetProductsByCategory
 {
     public class GetProductsByCategoryResponse
     {
-        
+        public IEnumerable<Shared.Models.Product> Products { get; internal set; }
     }
 }
